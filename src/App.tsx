63c94690cc4d50fc3dbd4ee820f722048f3a4682
Sharon-Shechter-HLS <sharons@moveohls.com>
2025-06-18 18:27:05 +0300

    feat : Added a magnifying glass icon

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import { Button } from "@/components/button/button"
+import { Input } from "@/components/ui/input"
 
 function App() {
   return (
     <div className="flex h-screen w-screen items-center justify-center">
       <div className="flex flex-col gap-4">
+        <Input placeholder="Search…" />
+
         <Button size="sm">Small</Button>
         <Button size="md">Medium</Button>
         <Button size="lg">Large</Button>
--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -1,21 +1,30 @@
 import * as React from "react"
-
 import { cn } from "@/lib/utils"
+import { Search } from "lucide-react"
 
-function Input({ className, type, ...props }: React.ComponentProps<"input">) {
-  return (
-    <input
-      type={type}
-      data-slot="input"
-      className={cn(
-        "file:text-foreground placeholder:text-muted-foreground selection:bg-primary selection:text-primary-foreground dark:bg-input/30 border-input flex h-9 w-full min-w-0 rounded-md border bg-transparent px-3 py-1 text-base shadow-xs transition-[color,box-shadow] outline-none file:inline-flex file:h-7 file:border-0 file:bg-transparent file:text-sm file:font-medium disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
-        "focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px]",
-        "aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive",
-        className
-      )}
-      {...props}
-    />
-  )
-}
+export interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {}
+
+const Input = React.forwardRef<HTMLInputElement, InputProps>(
+  ({ className, ...props }, ref) => {
+    return (
+      <div className="relative w-[293px] h-[38px]">
+        <Search className="absolute left-3 top-1/2 -translate-y-1/2 text-[#929AA3] size-4 pointer-events-none" />
+        <input
+          ref={ref}
+          className={cn(
+            "w-full h-full pl-10 pr-20 py-2 text-sm text-[#1c1e21] rounded-[8px] border border-[#929AA3] outline-none transition-all",
+            "hover:border-[#6b7280]",
+            "focus:border-[#131416]",
+            "disabled:text-[#cbd5e1] disabled:bg-[#f9fafb] disabled:border-[#e5e7eb]",
+            className
+          )}
+          {...props}
+        />
+      </div>
+    )
+  }
+)
+
+Input.displayName = "Input"
 
 export { Input }
