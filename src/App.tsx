c90a860e6f982bde9aebc932eb84eb2020ab12e2
Sharon-Shechter-HLS <sharons@moveohls.com>
2025-06-22 15:18:56 +0300

    feat: add ProgressBarRow component with dynamic width

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,16 @@
+import { ProgressBarRow } from "./components/progressBar/progressBarRow"
+
 function App() {
-  return 
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-100 p-8">
+      <div className="w-[250px] space-y-2 bg-white p-4 rounded-lg shadow">
+        <ProgressBarRow value={100} color="green" />
+        <ProgressBarRow value={60} color="yellow" />
+        <ProgressBarRow value={20} color="red" />
+        <ProgressBarRow value={100} color="gray" />
+      </div>
+    </div>
+  )
 }
 
 export default App
