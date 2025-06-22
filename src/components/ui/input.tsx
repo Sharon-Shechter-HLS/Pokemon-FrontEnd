ea866830e7528276633bd311bac0def964937e11
Sharon-Shechter-HLS <sharons@moveohls.com>
2025-06-22 12:35:44 +0300

    feat: Add a clear search X option

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -1,20 +1,37 @@
 import * as React from "react"
 import { cn } from "@/lib/utils"
-import { Search } from "lucide-react"
+import { Search, X } from "lucide-react"
 
 export interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {}
 
 const Input = React.forwardRef<HTMLInputElement, InputProps>(
   ({ className, ...props }, ref) => {
+    const [value, setValue] = React.useState("")
+
+    const handleClear = () => {
+      setValue("")
+    }
+
     return (
       <div className="relative w-[293px] h-[38px]">
         <Search className="absolute left-3 top-1/2 -translate-y-1/2 text-[#929AA3] size-4 pointer-events-none" />
+        {value && (
+          <button
+            type="button"
+            onClick={handleClear}
+            className="absolute right-3 top-1/2 -translate-y-1/2 text-[#929AA3] hover:text-[#6b7280] transition-colors"
+          >
+            <X className="size-4" />
+          </button>
+        )}
         <input
           ref={ref}
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
           className={cn(
-            "w-full h-full pl-10 pr-20 py-2 text-sm text-[#1c1e21] rounded-[8px] border border-[#929AA3] outline-none transition-all",
+            "w-full h-full pl-10 pr-10 py-2 text-sm text-[#1c1e21] rounded-[8px] border border-[#929AA3] outline-none transition-all",
             "hover:border-[#6b7280]",
-            "focus:border-[#131416]",
+            "focus:border-[#3B5AA6]",
             "disabled:text-[#cbd5e1] disabled:bg-[#f9fafb] disabled:border-[#e5e7eb]",
             className
           )}
