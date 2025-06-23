ff3e1b96754cd8656eb6fa4ba952783c04f2ed91
Sharon-Shechter-HLS <sharons@moveohls.com>
2025-06-23 21:15:43 +0300

    fix: PR feedback

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,7 @@
-import { ProgressBarRow } from "./components/progressBar/progressBarRow"
 
 function App() {
-  return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-100 p-8">
-      <div className="w-[250px] space-y-2 bg-white p-4 rounded-lg shadow">
-        <ProgressBarRow value={100} color="green" />
-        <ProgressBarRow value={60} color="yellow" />
-        <ProgressBarRow value={20} color="red" />
-        <ProgressBarRow value={100} color="gray" />
-      </div>
-    </div>
-  )
+  return
+
 }
 
 export default App
