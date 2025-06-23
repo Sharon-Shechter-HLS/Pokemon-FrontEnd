da7f60e686856a8d8607b078ba2663b001be015e
Sharon-Shechter-HLS <sharons@moveohls.com>
2025-06-23 17:00:06 +0300

    fix: PR feedback

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,5 @@
-import { Button } from "@/components/button/button"
-import { Input } from "@/components/ui/input"
 
-function App() {
-  return (
-    <div className="flex h-screen w-screen items-center justify-center">
-      <div className="flex flex-col gap-4">
-        <Input placeholder="Search…" />
+export default function App() {
 
-        <Button size="sm">Small</Button>
-        <Button size="md">Medium</Button>
-        <Button size="lg">Large</Button>
-
-        <Button variant="primary" disabled>
-          Primary Disabled
-        </Button>
-
-        <Button variant="secondary">Secondary</Button>
-        <Button variant="secondary" disabled>
-          Secondary Disabled
-        </Button>
-      </div>
-    </div>
-  )
+  return 
 }
-
-export default App
