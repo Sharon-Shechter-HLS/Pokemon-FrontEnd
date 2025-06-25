825c4ac4bc6915aa2ea5e3ed3d33b498f3653e26
Sharon-Shechter-HLS <sharons@moveohls.com>
2025-06-25 15:01:58 +0300

    feat: Card usind shadcn. new ui shadcn directory (select and card)

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,64 +1,29 @@
+import {
+  Card as ShadCard,
+  CardHeader,
+  CardContent,
+  CardFooter,
+} from "@/components/ui/card";
 
-type CardProps = {
+type GenericCardProps = {
   header: React.ReactNode;
   content: React.ReactNode;
   footer?: React.ReactNode;
 };
 
-function Card({ header, content, footer }: CardProps) {
+export default function Card({ header, content, footer }: GenericCardProps) {
   return (
-    <div
-      className="border border-gray-200 shadow-lg rounded-[4px] bg-white flex flex-col justify-between"
-      style={{ width: "502px", minHeight: "200px" }}
-    >
-      {/* Header */}
-      <div
-        style={{
-          width: "502px",
-          height: "86px",
-          padding: "12px 24px",
-          display: "flex",
-          alignItems: "center",
-          justifyContent: "space-between",
-        }}
-      >
-        {header}
-      </div>
+    <ShadCard >
+      <CardHeader>{header}</CardHeader>
 
-      {/* Content */}
-      <div
-        style={{
-          padding: "24px",
-          minHeight: "150px",
-          display: "flex",
-          flexDirection: "column",
-          alignItems: "center",
-          justifyContent: "center",
-        }}
-      >
-        {content}
-      </div>
+      <CardContent>{content}</CardContent>
 
-      {/* Footer */}
       {footer && (
-        <div
-          className="border-t"
-          style={{
-            width: "502px",
-            height: "72px",
-            padding: "16px",
-            gap: "16px",
-            display: "flex",
-            alignItems: "center",
-            justifyContent: "center",
-            borderTopWidth: "1px",
-          }}
-        >
-          {footer}
-        </div>
+        <>
+          <div className="w-full border-t my-2" /> 
+          <CardFooter className="justify-center gap-4">{footer}</CardFooter>
+        </>
       )}
-    </div>
+    </ShadCard>
   );
 }
-
-export default Card; 
