10538200f3aadcdea21ba78b9e5a5dd255831e6b
Sharon-Shechter-HLS <sharons@moveohls.com>
2025-07-01 19:23:07 +0300

    feat: routing to the pokemons pages

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,21 +1,19 @@
-import Button from "../Button/Button"
+import { useNavigate } from "react-router-dom"
+import Button from "../Button/button"
 
-type HeaderProps = {
-  onAllClick?: () => void
-  onMyClick?: () => void
-}
+export default function Header() {
+  const navigate = useNavigate() 
 
-export default function Header({ onAllClick, onMyClick }: HeaderProps) {
   return (
     <header className="w-full px-8 py-4 bg-white flex justify-between items-center shadow-sm">
       <div className="flex items-center gap-4">
         <img src="/Logo.png" alt="Pokemon Logo" className="h-[40px]" />
 
         <div className="flex gap-2 ml-4">
-          <Button variant="primary" size="md" onClick={onAllClick}>
+          <Button variant="primary" size="md" onClick={() => navigate("/allpokemon")}>
             All Pokemons
           </Button>
-          <Button variant="secondary" size="md" onClick={onMyClick}>
+          <Button variant="secondary" size="md" onClick={() => navigate("/mypokemon")}>
             My Pokemons
           </Button>
         </div>
