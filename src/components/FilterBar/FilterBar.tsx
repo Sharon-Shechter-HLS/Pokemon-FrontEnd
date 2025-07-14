e1e7510723a8adb3961397fcdea6971bc0bb4180
Sharon-Shechter-HLS <sharons@moveohls.com>
2025-07-14 13:05:02 +0300

    fix : PR changes

--- src/components/FilterBar/FilterBar.tsx
+++ src/components/FilterBar/FilterBar.tsx
@@ -1,58 +1,53 @@
 import * as React from "react";
 import { InputSearch } from "../input/InputSearch";
 import { Filter } from "../ui/filter";
-
-type HeadlineProps = {
-  children: React.ReactNode;
-  className?: string;
-};
-
-const Headline = ({ children, className = "" }: HeadlineProps) => (
-  <h1
-    className={`font-mulish font-medium leading-[34px] tracking-[0px] text-gray-900 ${className}`}
-  >
-    {children}
-  </h1>
-);
+import Headline from "../HeadLine/HeadLine";
 
 type FilterToolbarProps = {
   title: React.ReactNode;
   searchValue: string;
   onSearchChange: (value: string) => void;
-  options: Array<{ label: string; value: string }>;
   filterValue: string | null;
   onFilterChange: (value: string | null) => void;
-  filterLabel?: string;
   className?: string;
 };
 
-export const FilterBar = ({
+export const PageToolbar = ({
   title,
   searchValue,
   onSearchChange,
-  options,
   filterValue,
   onFilterChange,
-  filterLabel = "Filter",
-  className = "",
-}: FilterToolbarProps) => (
-  <div className={`w-full mb-4 ${className}`}>
-    <Headline className="text-2xl mb-4">{title}</Headline>
-    <div className="flex items-center justify-between bg-transparent rounded-md">
-      <InputSearch
-        placeholder="Search Pokemon"
-        value={searchValue}
-        onChange={onSearchChange}
-        size="md"
-      />
-      <Filter
-        options={options}
-        value={filterValue}
-        onChange={onFilterChange}
-        label={filterLabel}
-      />
-    </div>
-  </div>
-);
+}: FilterToolbarProps) => {
+  const filterLabel = "Sort By";
+  const filterOptions = [
+    { label: "Name (A-Z)", value: "name-asc" },
+    { label: "Name (Z-A)", value: "name-desc" },
+    { label: "Power (High-Low)", value: "power-desc" },
+    { label: "Power (Low-High)", value: "power-asc" },
+    { label: "HP (High-Low)", value: "hp-desc" },
+    { label: "HP (Low-High)", value: "hp-asc" },
+  ];
 
-export default FilterBar;
+  return (
+    <div className="w-full mb-4 ">
+      <Headline className="text-2xl mb-4">{title}</Headline>
+      <div className="flex items-center justify-between bg-transparent rounded-md">
+        <InputSearch
+          placeholder="Search Pokemon"
+          value={searchValue}
+          onChange={onSearchChange}
+          size="md"
+        />
+        <Filter
+          options={filterOptions}
+          value={filterValue}
+          onChange={onFilterChange}
+          label={filterLabel}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default PageToolbar;
